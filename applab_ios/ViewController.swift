506b591b5f0c1fdb973861efea95bf18b6499d34
Firas <Firas.Al-Doghman>
2021-04-28 18:42:53 +1000

    initial commit

---
+++ applab_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  applab_ios
+//
+//  Created by Firas Al-Doghman on 28/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
